f68baf36771e0fde67877b403f449cb67b7c11c2
peterferencz <ferenczp2005@gmail.com>
2024-03-16 21:38:44 +0100

    version 1.0.0

---
+++ generate.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require("fs")
+const path = require("path")
+
+const langs = {
+    "en": require("./lang/en.json"),
+    "hu": require("./lang/hu.json"),
+    "de": require("./lang/de.json")
+}
+
+const original = fs.readFileSync("index.html").toString()
+
+const generateLang = (lang) => {
+    console.log(lang)
+    l = langs[lang]
+
+    let text = original + ""
+    Object.keys(l).forEach(key => {
+        text = text.replaceAll(`|${key}|`, l[key])
+    });
+
+    text = text.replaceAll("|locale|", lang)
+    text = text.replaceAll(`|activateon-${lang}|`, "active")
+
+    fs.writeFileSync(path.join(__dirname, "docs", `${lang}.html`), text)
+}
+
+
+
+Object.keys(langs).forEach(lang => generateLang(lang))
+fs.cpSync(path.join(__dirname, "assets"), path.join(__dirname, "docs", "assets"), {recursive: true})
+fs.cpSync(path.join(__dirname, "style.css"), path.join(__dirname, "docs", "style.css"))
